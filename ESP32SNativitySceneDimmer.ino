74effbe79e229e7e9409d917f655b84a344941e6
VirtualFab <49951329+VirtualFab@users.noreply.github.com>
2021-12-21 07:25:54 +0100

    Add files via upload
    Added Bluetooth RemoteXY support

--- ESP32SNativitySceneDimmer.ino
+++ ESP32SNativitySceneDimmer.ino
@@ -1,3 +1,47 @@
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__ESP32CORE_BLUETOOTH
+#include <BluetoothSerial.h>
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_BLUETOOTH_NAME "RemoteXY"
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =
+  { 255,3,0,0,0,73,0,13,13,1,
+  2,0,21,3,22,11,2,26,31,31,
+  79,78,0,79,70,70,0,3,132,14,
+  28,33,9,2,26,129,0,14,20,34,
+  6,17,65,110,105,109,97,122,105,111,
+  110,101,0,4,192,0,49,62,7,2,
+  26,129,0,15,41,34,6,17,76,117,
+  109,105,110,111,115,105,116,195,160,0 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  uint8_t RaSwitch1; // =1 if switch ON and =0 if OFF 
+  uint8_t RaAnimazione; // =0 if select position A, =1 if position B, =2 if position C, ... 
+  int8_t slider_1; // =0..100 slider position 
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop)
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
@@ -5,7 +49,7 @@
 #define TIME_RES_MILLIS 10 
 
 // LED ------------------------------------------------------
-#define NUMLEDS 8
+#define NUMLEDS 7
 #define MAXANIMATIONSTEPS 10
 enum {
   ENDANIMATION=0,
@@ -15,19 +59,25 @@ enum {
   OFF
 };
 
-//uint8_t LedPin[NUMLEDS] ={2,15,5,18,19,21,36,39,32,33,25,26,27,14,12};
-uint8_t LedPin[NUMLEDS]   ={2,15,5,18,19,21,36,39};
+//uint8_t LedPin[NUMLEDS] ={2,15,5,18,19,21,22,23,32,33,25,26,27,14,12};
+uint8_t LedPin[NUMLEDS]   ={2,15,5,18,21,22,32};
 uint8_t intLedCurAniIdx[NUMLEDS];
 uint16_t intLedTimeCounter[NUMLEDS];
 uint8_t aLedProgram[][NUMLEDS][MAXANIMATIONSTEPS][2]={
 {
-   { {OFF, 1},{FADEUP,15},{ON, 1}, {FADEDOWN,15}, {OFF, 5} }
-  ,{ {OFF,32},{FADEUP,15},{ON, 1}, {FADEDOWN,15}, {OFF,96} }
-  ,{ {OFF,64},{FADEUP,35},{ON, 1}, {FADEDOWN,15}, {OFF,64} }
-  ,{ {OFF,96},{FADEUP,45},{ON, 1}, {FADEDOWN,15}, {OFF,32} }
+   { {FADEUP,10}, {ON,75}, {FADEDOWN,5}, {OFF, 30} }
+  ,{ {OFF,10}, {FADEUP,10},{ON, 55}, {FADEDOWN,5}, {OFF,35} }
+  ,{ {OFF,20}, {FADEUP,10},{ON, 40}, {FADEDOWN,5}, {OFF,45} }
+  ,{ {OFF,30}, {FADEUP, 5},{ON, 30}, {FADEDOWN,5}, {OFF,50} }
+  ,{ {OFF,35}, {FADEUP, 5},{ON, 25}, {FADEDOWN,5}, {OFF,50} }
+  ,{ {OFF,40}, {FADEUP, 5},{ON, 20}, {FADEDOWN,5}, {OFF,50} }
+  ,{ {OFF,45}, {FADEUP, 5},{ON, 25}, {FADEDOWN,5}, {OFF,40} }
 }
 ,{
    { {ON, 1},{OFF, 1} }
+  ,{ {ON, 1},{OFF, 1} }
+  ,{ {ON, 1},{OFF, 1} }
+  ,{ {ON, 1},{OFF, 1} }
   ,{ {ON, 1},{OFF, 1} }
   ,{ {ON, 1},{OFF, 1} }
   ,{ {ON, 1},{OFF, 1} }
@@ -37,19 +87,31 @@ uint8_t aLedProgram[][NUMLEDS][MAXANIMATIONSTEPS][2]={
   ,{ {FADEUP, 5},{FADEDOWN, 5} }
   ,{ {FADEUP, 5},{FADEDOWN, 5} }
   ,{ {FADEUP, 5},{FADEDOWN, 5} }
+  ,{ {FADEUP, 5},{FADEDOWN, 5} }
+  ,{ {FADEUP, 5},{FADEDOWN, 5} }
+  ,{ {FADEUP, 5},{FADEDOWN, 5} }
+}
+,{
+   { {ON, 1} }
+  ,{ {ON, 1} }
+  ,{ {ON, 1} }
+  ,{ {ON, 1} }
+  ,{ {ON, 1} }
+  ,{ {ON, 1} }
+  ,{ {ON, 1} }
 }
 };
 #define NUMLEDPROGRAMS sizeof(aLedProgram)/NUMLEDS/MAXANIMATIONSTEPS/2
 
-uint8_t intLedCurPgm=0;
-//uint8_t LedFadeSpeed=10;
+uint8_t intLedCurPgm=1;
 float LedPWM[NUMLEDS];
 float LedFadeIncrement[NUMLEDS];
 
-/* LED PWM Settings */
-#define PWMFREQ 5000 /* 5 KHz */
-#define PWMRESOLUTION 10
+// LED PWM Settings 
+#define PWMFREQ 5000 // 5 KHz 
+#define PWMRESOLUTION 10 // 0 to 2^10-1=1023
 const int MAX_DUTY_CYCLE = (int)(pow(2, PWMRESOLUTION) - 1);
+int intBrightness;
 
 // PUSHBUTTON ----------------------------------------------
 #define PUSHBUTTONPIN 0 //13 // use 0 for GPIO0, the on-board pushbutton
@@ -70,8 +132,8 @@ DallasTemperature sensors(&oneWire);
 float temperatureC;
 
 // FAN ------------------------------------------------------
-#define FAN_PIN 32 
-#define FAN_PWMCHAN 15       // use the last one, leaving all the other 15 to LEDs
+#define FAN_PIN 33
+#define FAN_PWMCHAN 15       // use the last one, leaving all the other 0 to 15 to LEDs
 #define FAN_PWM_RESOLUTION 6 // 64 different fan speeds are enough
 #define FAN_PWM_FREQ 10000   // Use a higher frequency for the fan, otherwise you can hear it whisteling
 const int FAN_PWM_MAX=(pow(2, FAN_PWM_RESOLUTION) - 1);
@@ -128,22 +190,26 @@ void setup() {
   sensors.setWaitForConversion(false); // Function requestTemperature() etc returns immediately 
 
   pinMode(PUSHBUTTONPIN, INPUT);
+
+  RemoteXY_Init (); 
+
   dbgprintf("READY");
-  dbgprintf("intAniSize0=%i",NUMLEDPROGRAMS);
 }
 
-boolean blnEnabled=false;
+//boolean blnEnabled=false;
 
 // ------------------------------------------------------------------------------
 void loop() {
 // ------------------------------------------------------------------------------
 #define PUSHBUTTONLONGPRESS 200 // 2 seconds for a long press state
+
+  RemoteXY_Handler();
   
   if(millis()-lastTimerValue>=10) { // 10 milliseconds=1 hundredth of a second
     lastTimerValue=millis();
     intSecHundr++;
 
-    // Push button handling -----------------------------------------
+    /*// Push button handling -----------------------------------------
     int intPushButtonState = digitalRead(PUSHBUTTONPIN);
     if(intPushButtonState != intPushButtonPrevState) { 
       dbgprintf("Button changed state: %i", intPushButtonState);
@@ -167,9 +233,17 @@ void loop() {
           blnEnabled = true; 
         }
       }
-    }
+    }*/
 
-    if(blnEnabled) {
+
+    if(RemoteXY.RaSwitch1==0) { // Power OFF
+      LedProgramInit(); // shut down LEDs
+    } else {
+      if(intLedCurPgm != RemoteXY.RaAnimazione) {
+        LedProgramInit(); 
+      }
+      intLedCurPgm=RemoteXY.RaAnimazione;
+      intBrightness=MAX_DUTY_CYCLE*(100-RemoteXY.slider_1)/100;
       // LED handling -------------------------------------------------
       for(uint8_t i=0;i<NUMLEDS;i++) {
         if(intLedTimeCounter[i]>0) {
@@ -179,7 +253,7 @@ void loop() {
               intLedCurAniIdx[i]=0;
             } 
             intLedTimeCounter[i]=aLedProgram[intLedCurPgm][i][intLedCurAniIdx[i]][1]*100; // Seconds->Hundredths
-            LedFadeIncrement[i]=MAX_DUTY_CYCLE/(((float)intLedTimeCounter[i]/TIME_RES_MILLIS*10));
+            LedFadeIncrement[i]=intBrightness/(((float)intLedTimeCounter[i]/TIME_RES_MILLIS*10));
             if(i==0){
               dbgprintf("Led %i changed animation step: %i Counter=%i FadeIncrement=%f", i, intLedCurAniIdx[i], intLedTimeCounter[i], LedFadeIncrement[i]);
             }
@@ -190,13 +264,13 @@ void loop() {
             LedPWM[i]=0;
             break;
           case ON:
-            LedPWM[i]=MAX_DUTY_CYCLE;
+              LedPWM[i]=intBrightness;
             break;
           case FADEUP:
-            if(MAX_DUTY_CYCLE-LedPWM[i]>LedFadeIncrement[i]) {
+            if(intBrightness-LedPWM[i]>LedFadeIncrement[i]) {
               LedPWM[i]+=LedFadeIncrement[i];
             } else {
-              LedPWM[i]=MAX_DUTY_CYCLE;
+              LedPWM[i]=intBrightness;
             }
             break;
           case FADEDOWN:
